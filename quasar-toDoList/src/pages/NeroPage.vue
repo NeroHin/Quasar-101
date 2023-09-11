14a9add1440991c209be778ac6c23694cbadb197
NeroUcH <hin1997101@gmail.com>
2023-09-12 00:51:50 +0800

    feat: create a page and softlink router

---
+++ quasar-toDoList/src/pages/NeroPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>Hi, Here is Nero, I have learning Quasar with Vue.js</h3>
+  </div>
+</template>
+
+<script>
+import { reactive, toRefs } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0,
+    });
+
+    return {
+      ...toRefs(state),
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
